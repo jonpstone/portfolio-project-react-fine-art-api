22c42b8a8d48fd4330b2f629eaa7e1aae47a3051
Jonathan Peter Stone <github email address>
2018-06-09 15:12:00 -0500

    Created folders for components and reducers.

---
+++ client/src/components/paintings_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PaintingsIndex extends Component {
+  render() {
+    return (
+      <div>
+        Paintings index
+      </div>
+    );
+  }
+}
+
+export default PaintingsIndex;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
+import PaintingsIndex from './components/paintings_index';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={PaintingsIndex} />
+    </div>
+  </BrowserRouter>
+);
 registerServiceWorker();
