bd3883fca35cfd34c851decec84743abb6c816fa
Jonathan Stone <jpstone@protonmail.com>
2018-10-12 18:27:04 -0500

    Started styles for header.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,11 +3,15 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <h1>Oscar Henry</h1>
-    <h3>House of Fine Art</h3>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-    <NavLink to="/periods" activeClassName="is-active">Periods</NavLink>
-    <NavLink to="/about" activeClassName="is-active">About</NavLink>
+    <div className="header">
+      <h1 className="header_title">Oscar Henry</h1>
+      <h3 className="header_subtitle">House of Fine Art</h3>
+      <div className="nav_links">
+        <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+        <NavLink to="/periods" activeClassName="is-active">Periods</NavLink>
+        <NavLink to="/about" activeClassName="is-active">About</NavLink>
+      </div>
+    </div>
   </header>
 );
 
