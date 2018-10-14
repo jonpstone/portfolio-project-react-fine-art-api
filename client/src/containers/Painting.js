2d644558536756c93b68aad2d0bb366fd9c6a472
Jonathan Stone <jpstone@protonmail.com>
2018-10-14 05:46:56 -0500

    Completed asynch lifecycle method for Painting.

--- client/src/containers/Painting.js
+++ client/src/containers/Painting.js
@@ -3,19 +3,31 @@ import React, { Component } from 'react';
 export class Painting extends Component {
   constructor(props) {
     super(props);
-    console.log("painting", props);
     this.state = {
-
+      paintingFetchId: props.match.params.id,
+      painting: []
     };
+  }
+
+  componentDidMount() {
+    console.log("urlId", this.state.paintingFetchId);
+
+    fetch(`/api/paintings/${this.state.paintingFetchId}`)
+    .then(res => {
+      return res.json();
+    }).then(data => {
+      this.setState({ painting: data })
+    })
   }
 
   render() {
     return (
       <div>
-        {this.state.painting}
+        {console.log(this.state.painting)}
       </div>
     );
   }
 }
 
 export default Painting;
+
